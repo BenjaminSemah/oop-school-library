c023468a66bcfc78112edfb2e3423313a9b716b3
Benjamin Semah <benjaminsemah@gmail.com>
2022-04-13 15:10:24 +0000

    Create many-to-many relatioinship between Person and Book

--- rental.rb
+++ rental.rb
@@ -1,7 +1,14 @@
 class Rental
+  attr_reader :person, :book
   attr_accessor :date
 
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
+
+    @book = book
+    book.rentals << self
+
+    @person = person
+    person.rentals << self
   end
 end
